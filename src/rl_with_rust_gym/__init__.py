c222588a842e06baaa2da685c92272e9f207bdf6
SverreNystad <sverrenystad@gmail.com>
2026-09-20 11:23:00 +0200

    feat: add train function and integrate it into the main script

--- src/rl_with_rust_gym/__init__.py
+++ src/rl_with_rust_gym/__init__.py
@@ -1,5 +1,6 @@
 from .rl_with_rust_gym import *
 
+from .trainer import *
 
 __doc__ = rl_with_rust_gym.__doc__
 if hasattr(rl_with_rust_gym, "__all__"):
--- trainer.py
+++ trainer.py
@@ -1,5 +1,8 @@
-from rl_with_rust_gym import sum_as_string
+from rl_with_rust_gym import sum_as_string, train
 
 
 if __name__ == "__main__":
     print(sum_as_string(1, 3))
+
+    # Run the training function
+    train()
